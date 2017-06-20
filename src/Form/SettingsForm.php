beb4fff1d903ee64262ee1f9966359e1b601d413
inghamn <inghamn@bloomington.in.gov>
2017-06-20 10:12:47 -0400

    Base route is now configurable
    Fixes #5

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -44,6 +44,12 @@ class SettingsForm extends ConfigFormBase
             '#default_value' => $config->get('promt_breadcrumb'),
             '#description'   => 'Enter node IDs, separated by commas, to be used as the base breadcrumb for Promt routes'
         ];
+        $form['promt_route'] = [
+            '#type'          => 'textfield',
+            '#title'         => 'Base Route',
+            '#default_value' => $config->get('promt_route'),
+            '#description'   => "Specify an alias to use as the base for this module's routes"
+        ];
 
         return parent::buildForm($form, $form_state);
     }
@@ -54,8 +60,9 @@ class SettingsForm extends ConfigFormBase
     public function submitForm(array &$form, FormStateInterface $form_state)
     {
         $this->config('promt.settings')
-             ->set('promt_url',        $form_state->getValue('promt_url'))
+             ->set('promt_url',        $form_state->getValue('promt_url'       ))
              ->set('promt_breadcrumb', $form_state->getValue('promt_breadcrumb'))
+             ->set('promt_route',      $form_state->getValue('promt_route'     ))
              ->save();
 
         parent::submitForm($form, $form_state);
